30f0bc6fc3b373fb0f479e7adba6bf246cbe12d0
ftforweb <ftforweb@gmail.com>
2018-10-02 12:27:18 +0400

    test

---
+++ test/assets/js/app.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+  $('.block-1 .owl-carousel').owlCarousel({
+    items : 1,
+    navigation : true,
+    pagination: false,
+    nav: false,
+    loop: true,
+    autoplay: true,
+    autoplayTimeout: 4000
+  });
+});
